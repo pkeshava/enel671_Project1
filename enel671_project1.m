7de628494fd0363ccd2ee326ce0ba012786b5452
Pouyan <pouyank13@me.com>
2016-10-16 17:12:34 -0600

    complete first version of part1 and 2 for computing of MMSE for each channel

--- enel671_project1.m
+++ enel671_project1.m
@@ -15,6 +15,7 @@
 % Define Noise matrix 
 sigma_squared = 0.0001;
 Rv = sigma_squared*eye(11);
+
 % Define impulse response matrix of channels
 h = [0.2194 1.0 0.2194;0.2798 1.0 0.2798;0.3365 1.0 0.3365;0.3887 1.0 0.3887];
 % calculate values of r for each channel
@@ -23,24 +24,60 @@ r = autocorrelation_values(h);
 [R2, max2, min2, spread2] = autocorrelation_eigen(r(:,2),Rv);
 [R3, max3, min3, spread3] = autocorrelation_eigen(r(:,3),Rv);
 [R4, max4, min4, spread4] = autocorrelation_eigen(r(:,4),Rv);
+
 %% Generate sequence of BPSK data source
-a = round(rand(1,1000));
-n = size(a,2);
-for i=1:n
-    if a(i) == 0
-        a(i) = -1;
+% Filter order M
+M = 11;
+% Step size mu
+mu = 0.075;
+% Data length N
+N = 1000;
+% Define delay as seen by the desired signal
+delta=(M-1)/2 + (length(h(1,:))-1)/2;
+% Number of iterations for experiment
+K = 500;
+for k=1:K
+    a = round(rand(1,N));
+    for i=1:N
+       if a(i) == 0
+           a(i) = -1;
+        end
     end
-end
-%% Calculate u(n)
+% Calculate u(n)
 u = filterinput(a,h);
-%% Effect of Eignevalue Spread
+
+% Effect of Eignevalue Spread
 % Generate and plot the learning curves for Channels 1,2,3,4
 
-% Run data vector through LMS algorithm 
 
+% Run data vector through LMS algorithm for each channel and get calculated
+% error and weight vector
+[e1,W1] = LMS_P1(u(:,1),a,mu,delta,M);
+ed1(:,k) = e1.^2;
+MSEE1 = sum(ed1,2)/K;
+[e2,W2] = LMS_P1(u(:,2),a,mu,delta,M);
+ed2(:,k) = e2.^2;
+MSEE2 = sum(ed2,2)/K;
+[e3,W3] = LMS_P1(u(:,3),a,mu,delta,M);
+ed3(:,k) = e3.^2;
+MSEE3 = sum(ed3,2)/K;
+[e4,W4] = LMS_P1(u(:,4),a,mu,delta,M);
+ed4(:,k) = e4.^2;
+MSEE4 = sum(ed4,2)/K;
+end 
+%% Plot it
+% 
+for MSEE = [MSEE1 MSEE2 MSEE3 MSEE4]
+    figure(1)
+    semilogy(1:1000,MSEE,'LineWidth',2)
+    legend('Channel 1','Channel 2','Channel 3','Channel 4')
+    grid on
+    xlabel('Time (s)');
+    ylabel('Mean Squared Error');
+    hold on
+end
 %% Effect of Filter Order
-% Generate and plot MSE with oder M = 9, 11, 21
-
+% Generate and plot MSE with oder M = 9, 11, 21 for channel 2
 
 
 
